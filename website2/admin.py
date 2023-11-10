861e9b3991ef7bd1e4509836f2b58a4c6b52eeb2
nakimulimariam <mariamnakimuli602@gmail.com>
2023-11-10 15:23:27 -0800

    changes made in models, admin

--- website2/admin.py
+++ website2/admin.py
@@ -1,3 +1,23 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import EquipmentCategory, Product, Transaction
+
+class EquipmentCategoryAdmin(admin.ModelAdmin):
+    list_display = ('category_name', 'date_created')
+    list_filter = ('date_created',)
+    search_fields = ('category_name',)
+
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ('category_id', 'product_name', 'unit_price', 'sale_price', 'available_stock', 'unit_measure', 'date_updated')
+    list_filter = ('date_updated',)
+    search_fields = ('category_id', 'product_name')
+    
+class TransactionAdmin(admin.ModelAdmin):
+    list_display = ('product_id', 'transaction_type', 'stock_taken', 'transaction_amount', 'transaction_date')
+    list_filter = ('transaction_date',)
+    search_fields = ('product_id', 'transaction_type')
+
+admin.site.register(EquipmentCategory, EquipmentCategoryAdmin)
+admin.site.register(Product, ProductAdmin)
+admin.site.register(Transaction, TransactionAdmin)
+
